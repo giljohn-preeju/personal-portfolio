eb2b1d4409b8959f8b5aa071ed240561256f70fc
giljohn-preeju <63243478+giljohn-preeju@users.noreply.github.com>
2023-08-01 18:29:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+        document.getElementById("arrow").style.border = "2px solid white";
+    } else {
+        mybutton.style.display = "none";
+        document.getElementById("arrow").style.border = "2px solid transparent";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
